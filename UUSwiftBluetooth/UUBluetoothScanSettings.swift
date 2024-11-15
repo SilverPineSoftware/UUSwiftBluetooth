5285477ac14c13961a8a38c966beb742e28d6e03
Ryan DeVore <ryan@silverpine.com>
2024-11-15 08:02:52 -0800

    add comments

--- UUSwiftBluetooth/UUBluetoothScanSettings.swift
+++ UUSwiftBluetooth/UUBluetoothScanSettings.swift
@@ -8,27 +8,66 @@
 import UIKit
 import CoreBluetooth
 
+/**
+ Contains BLE scanning settings
+ */
 public struct UUBluetoothScanSettings
 {
-   public init(allowDuplicates: Bool = false,
-                serviceUUIDs: [CBUUID]? = nil,
-                discoveryFilters: [UUPeripheralFilter]? = nil,
-                outOfRangeFilters: [UUOutOfRangePeripheralFilter]? = nil,
-                outOfRangeFilterEvaluationFrequency: TimeInterval = 0.5,
-                scanWatchdogTimeout: TimeInterval = 0.5)
+    /**
+     Creates an instance of UUBluetoothScanSettings
+     */
+    public init()
     {
-        self.allowDuplicates = allowDuplicates
-        self.serviceUUIDs = serviceUUIDs
-        self.discoveryFilters = discoveryFilters
-        self.outOfRangeFilters = outOfRangeFilters
-        self.outOfRangeFilterEvaluationFrequency = outOfRangeFilterEvaluationFrequency
-        self.scanWatchdogTimeout = scanWatchdogTimeout
+        
     }
     
+    /**
+     Passed into the CBCentralManager scanForPeripherals for the CBCentralManagerScanOptionAllowDuplicatesKey options key.  When true,
+     Core Bluetooth will give a callback for every BLE advertisement.  When false, Core Bluetooth will aggregate advertisements.  This perameter
+     is only valid when the app is in the foreground.
+     */
     public var allowDuplicates: Bool = false
+    
+    /**
+     List of BLE services to scan for.
+     */
     public var serviceUUIDs: [CBUUID]? = nil
+    
+    /**
+     List of optional filters to apply when scanning.  Typical filters check things like RSSI or manufacturing data.  These filters determine whether or not
+     a peripheral is to be added to the 'nearby' peripherals list.  Once a peripheral meets the criteria to be considered 'nearby', then all subsequent advertisements
+     are processed.  A peripheral can only be removed from the 'nearby' list by meeting one or more of the out of range filters.
+     */
     public var discoveryFilters: [UUPeripheralFilter]? = nil
+    
+    /**
+     List of optional filters to apply when determining if a peripheral is 'out of range'.  These are often related to the discovery filters.  These filters are evaluated
+     on a timer while scanning.  If any peripherals change from in range to out of range, the nearby devices callback is invoked with the updated list.
+     */
     public var outOfRangeFilters: [UUOutOfRangePeripheralFilter]? = nil
+    
+    /**
+     Frequency (in seconds) at which the out of range filters are evaluated
+     
+     Note: if this value is less than or equal to zero, the out of range filter logic is disabled.
+     */
     public var outOfRangeFilterEvaluationFrequency: TimeInterval = 0.5
-    public var scanWatchdogTimeout: TimeInterval = 0.0
+    
+    /**
+     Simulated ranging makes UUSwiftBluetooth stop and re-start the BLE scan under the hood as a way attempt to collect
+     real time advertisements from BLE peripherals.  When allowDuplicates is false, a normal CoreBluetooth scan will aggregate
+     BLE advertisements, so each invocation of scanForPeripherals might only report a single advertisement per peripheral.
+     
+     Note: when allowDuplicates is true, simulatedRanging is ignored
+     */
+    public var simulatedRanging: Bool = false
+    
+    /**
+     Simulated ranging involves stopping and restarting the CoreBluetooth scan.  This watchdog timeout value controls how often that
+     restart occurs.  Each time a peripheral is discovered, this watchdog timer is kicked.  If the timer fires, then the scan is stopped and
+     restarted.  If this value is less than or equal to zero, simulated ranging is ignored.
+     
+     Note: when allowDuplicates is true, simulatedRanging is ignored
+     */
+    public var simulatedRangingWatchdogTimeout: TimeInterval = 0.0
 }
