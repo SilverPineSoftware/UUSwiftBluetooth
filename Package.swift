7d5bb80da0870216e46c51e10247f961f98f99cc
Ryan DeVore <ryan@silverpine.com>
2024-11-17 13:53:48 -0800

    fixing branch ref

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
 		// and from where they can be fetched:
 		.package(
 			url: "https://github.com/SilverPineSoftware/UUSwiftCore.git",
-            from: .branch("develop")
+            .branch("develop")
 		)
 	],
 
