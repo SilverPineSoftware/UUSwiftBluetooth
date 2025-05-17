d02d591b28d5ccb6a9a06901cb6e6a5206f93507
Ryan DeVore <ryan@silverpine.com>
2025-05-17 16:19:11 -0700

    try adding dynamic type hook

--- Package.swift
+++ Package.swift
@@ -1,7 +1,26 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import class Foundation.ProcessInfo
 import PackageDescription
+
+func libraryType() -> PackageDescription.Product.Library.LibraryType?
+{
+    if let type = ProcessInfo.processInfo.environment["UU_LIBRARY_TYPE"]
+    {
+        if type == "static"
+        {
+            return .static
+        }
+        else if type == "dynamic"
+        {
+            return .dynamic
+        }
+    }
+
+    return nil
+}
+
 
 let package = Package(
 	name: "UUSwiftBluetooth",
@@ -14,6 +33,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "UUSwiftBluetooth",
+            type: libraryType()
 			targets: ["UUSwiftBluetooth"]),
 	],
 
@@ -31,7 +51,7 @@ let package = Package(
             name: "UUSwiftBluetooth",
             dependencies: ["UUSwiftCore"],
             path: "UUSwiftBluetooth",
-            exclude: ["Info.plist"])
+            exclude: ["Info.plist", "UnitTests"])
 	],
     swiftLanguageModes: [
 		.v4_2,
