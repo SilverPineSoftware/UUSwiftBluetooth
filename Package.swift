ff97aa9ba3de1a4fb7f84b5b853df6755cf95f76
Ryan DeVore <ryan@silverpine.com>
2025-05-16 22:52:08 -0700

    Remove dynamic

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "UUSwiftBluetooth",
-            type: .dynamic,
 			targets: ["UUSwiftBluetooth"]),
 	],
 
