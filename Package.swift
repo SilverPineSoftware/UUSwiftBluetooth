4eda78cf4799edeadd12942b63d3a7c8d594f32d
ryandevore <ryan@silverpine.com>
2026-09-15 18:36:33 -0700

    let xcode do it’s thing
    + update packages

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
 		// and from where they can be fetched:
 		.package(
             url: "https://github.com/SilverPineSoftware/UUSwiftCore.git",
-            from: "1.5.0"
+            from: "1.7.1"
 		)
 	],
 
